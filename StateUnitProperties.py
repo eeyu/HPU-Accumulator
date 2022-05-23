f974b4397c36657c048a2750aaf66c795666ff28
Evan <eyu@ihmc.org>
2022-05-23 13:25:31 -0500

    updated comments

--- StateUnitProperties.py
+++ StateUnitProperties.py
@@ -1,3 +1,5 @@
+# All math is done in SI units. these properties can be used to scale to human-preferred units
+
 class StateUnitProperties:
     def __init__(self, name : str, SIUnits : str, preferredUnits : str, scalingSIToPreferred):
         self.name = name
